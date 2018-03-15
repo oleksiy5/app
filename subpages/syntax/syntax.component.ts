d7313cf7977bb59e52a51aa19bab37d6188514a5
onekrasov <onekrasov@sygnity.pl>
2018-03-15 07:02:30 +0100

    qwe

--- subpages/syntax/syntax.component.ts
+++ subpages/syntax/syntax.component.ts
@@ -1,5 +1,6 @@
 import { Component, Sanitizer } from '@angular/core';
-import {DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DESTRUCTION } from 'dns';
 
 
 @Component({
@@ -7,24 +8,71 @@ import {DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   templateUrl: './syntax.component.html'
 })
 
-export class SyntaxComponent  {
+export class SyntaxComponent {
   isVisible: boolean = false;
-  changeValue() {    
+  changeValue() {
     this.isVisible = !this.isVisible;
   }
 
-  constructor(private sanitizer: DomSanitizer){
+  public userAnswer: String;
+  public fontAnswer: String;
+  setUserAnswer(ans: boolean) {    
+      if(ans){
+          this.userAnswer = "dobrze";
+          this.fontAnswer = "green";
+      }
+      else{
+          this.userAnswer = "źle";
+          this.fontAnswer = "red";
+      }
+  }
 
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   public lessons: typeLesson[] = [
-    {title: 'Programowanie C# - pierwszy krok', 
-    videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3wHZQYyQ2is')},
-     {title: 'debug aplikacji i przydatne klawisze F5, F10, F11', 
-     videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/91GDnzrsZZg')}
+    {
+      title: 'Programowanie C# - pierwszy krok',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3wHZQYyQ2is'),
+      quiz: {
+             question: 'Programując w C#, jakie jest podstawowe narzędzie?',
+             answers: [
+                { value: 'A', display: 'Google', isCorrect: false},
+                { value: 'B', display: 'Notatnik', isCorrect: false }, 
+                { value: 'C', display: 'Visual Studio', isCorrect: true }  
+            ]                                            
+      } 
+    },
+    {
+      title: 'debug aplikacji i przydatne klawisze F5, F10, F11',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/91GDnzrsZZg'),
+      quiz: {
+             question: 'Co to jest debug aplikacji?',
+             answers: [
+                { value: 'A', display: '.', isCorrect: true},
+                { value: 'B', display: '..', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    }
+    //,
+    // {
+    //   title: 'debug aplikacji i przydatne klawisze F5, F10, F11',
+    //   videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/91GDnzrsZZg')
+    // }
   ];
 }
 
-export interface typeLesson{
-  title:string; videoUrl: SafeResourceUrl
+export interface typeLesson {
+  title: string; videoUrl: SafeResourceUrl; quiz: typQuiz;
+}
+
+export interface typQuiz
+{
+  question: string, answers: Array<typAnswer>
+}
+
+export interface typAnswer
+{
+  value: string; display: string; isCorrect: boolean
 }
