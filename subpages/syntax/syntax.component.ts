8af48afc6b92dabef9fd05f77c426e23a77cbe75
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-03-15 16:32:17 +0100

    add YT video

--- subpages/syntax/syntax.component.ts
+++ subpages/syntax/syntax.component.ts
@@ -17,7 +17,7 @@ export class SyntaxComponent {
   }
 
   public lessons: typeLesson[] = [
-    {
+    {      
       title: 'Programowanie C# - pierwszy krok',
       videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3wHZQYyQ2is'),
       quiz: {            
@@ -38,19 +38,241 @@ export class SyntaxComponent {
       quiz: {           
              userAnswer: "",
              fontAnswer: "",
-             question: 'Co to jest debug?',
+             question: 'Co to jest debug aplikacji?',
              answers: [
-                { value: 'A', display: '.', isCorrect: true},
-                { value: 'B', display: '..', isCorrect: false }, 
+                { value: 'A', display: 'Proces poszukiwania błędów', isCorrect: true},
+                { value: 'B', display: 'To z angielskiego the bug [de-bug]', isCorrect: false }, 
+                { value: 'C', display: 'Domena aplikacji rozproszonych', isCorrect: false }  
+            ]                                            
+      } 
+    }
+    ,
+    {
+      title: 'warunek "if"',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/dqR202ecsOI'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
                 { value: 'C', display: '...', isCorrect: false }  
             ]                                            
       } 
     }
-    //,
-    // {
-    //   title: 'debug aplikacji i przydatne klawisze F5, F10, F11',
-    //   videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/91GDnzrsZZg')
-    // }
+    ,
+    {
+      title: 'pętle "for" i "foreach"',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/R1nBLhmOaNM'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'kolekcje np. "List"',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/HTg-wC_AdqA'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Klasy i Obiekty - na przykładzie',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/TNaMEtZ6ld4'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Ćwiczenie Klasy i Obiekty',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Z1oAmATQjWQ'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } 
+    ,
+    {
+      title: 'Metody i Konstruktory',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/4C28M_ANIIo'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: '[Ćwicz C#] Metody  i  Konstruktory',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/lLTzdYvEYNA'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Zmienne',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Z6ZpYuUZ8zI'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Właściwości',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/69XAF6YAEwk'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Ćwicz C#\'pa zmienne i właściwości',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/tCw8XlSErlY'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'modyfikatory dostępu',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/CWlLCqU1GiQ'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Ćwicz C#\'a modyfikatory dostępu',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/c1OsY4eveGU'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Rozwiązanie zadania z video Ćwicz C#\'a modyfikatory dostępu"',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/iwRu2_Z8xzU'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Zadanie końcowe',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/jL9_9xTqqlg'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'Rozwiązanie zadania końcowego cz. 1',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/c2N4JB8d5lo'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    } ,
+    {
+      title: 'rozwiązanie zadania końcowego cz. 2',
+      videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/WvS0T-RqBFc'),
+      quiz: {           
+             userAnswer: "",
+             fontAnswer: "",
+             question: '...?',
+             answers: [
+                { value: 'A', display: '...', isCorrect: true},
+                { value: 'B', display: '...', isCorrect: false }, 
+                { value: 'C', display: '...', isCorrect: false }  
+            ]                                            
+      } 
+    }
   ];
 }
 
@@ -65,5 +287,5 @@ export interface typQuiz
 
 export interface typAnswer
 {
-  value: string; display: string; isCorrect: boolean; //userAnswer: boolean
+  value: string; display: string; isCorrect: boolean;
 }
