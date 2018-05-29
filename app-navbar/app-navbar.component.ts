409be54b7325710e887d5984406b5c3d8b2689a1
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-05-29 17:04:01 +0200

    d

--- app-navbar/app-navbar.component.ts
+++ app-navbar/app-navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import {LanguageService} from '../app.languageService';
+//import {LanguageService} from '../app.languageService';
 
 @Component({
   selector: 'app-navbar',
@@ -12,29 +12,27 @@ export class AppNavbarComponent implements OnInit {
   //constructor() { }
 
   constructor(private translateService: TranslateService) {
-    //translateService.setDefaultLang('pl');
-    // translateService.setDefaultLang(languageService.getLanguage());
-    // translateService.use(languageService.getLanguage());
-    var lang = localStorage.getItem('lang'); 
-    if(lang){
-      //translateService.setDefaultLang(localStorage.getItem('lang'));
+
+    var lang = localStorage.getItem('lang');
+    if (lang) {
       translateService.use(localStorage.getItem('lang'));
-  }else {
-    //translateService.setDefaultLang('pl');
-    translateService.use('pl');
-       localStorage.setItem("lang","pl");
+    } 
+    else {
+      translateService.use('pl');
+      localStorage.setItem("lang", "pl");
+    }
   }
-  }
+
   switchLanguage(language: string) {
-    localStorage.setItem("lang",language);
-    var lang =localStorage.getItem("lang");
-    //this.translateService.setDefaultLang(lang);
+    localStorage.setItem("lang", language);
+    var lang = localStorage.getItem("lang");
+    this.translateService.setDefaultLang(lang);
     this.translateService.use(lang);
+    // console.log(lang);
   }
 
   ngOnInit() {
-    
+
   }
 
 }
-  
